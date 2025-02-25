0a9b22ab4390fdbb84e4e5c929e93ba8a1901ff4
KPrasch <kieranprasch@gmail.com>
2025-02-25 14:31:50 -0700

    disables image optimization

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
   },
   output: 'export',  // Enable static exports
   distDir: 'out',    // Explicitly set output directory to 'out'
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 module.exports = nextConfig; 
